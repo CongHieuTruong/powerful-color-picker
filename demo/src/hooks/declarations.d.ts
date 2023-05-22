3962cc3a3f44e8e8c0d38f70f7275469a9d7c31b
Hieu Truong <hieu.tnxd.bk@gmail.com>
2023-05-22 19:57:24 +0700

    commit

--- demo/src/hooks/declarations.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "use-throttled-effect" {
-  export default function useThrottledEffect(
-    callback: React.EffectCallback,
-    delay: number,
-    deps?: React.DependencyList | undefined
-  ): void;
-}
--- demo/src/hooks/useBodyBackground.ts
+++
@@ -1,7 +0,0 @@
-import { useEffect } from "react";
-
-export const useBodyBackground = (color: string): void => {
-  useEffect(() => {
-    document.body.style.backgroundColor = color;
-  }, [color]);
-};
